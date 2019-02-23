ba31dbbea1e7123f464cf899f4d4939bf9a05861
Kolya <nicolay.crasicov@gmail.com>
2019-02-23 23:44:45 +0200

    Приложение. Часть 1

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,25 +1,75 @@
 "use strict";
 
 let money = +prompt("Ваш бюджет на месяц?", ""),
-    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
-    itemExpensesFirst = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    costItemExpensesFirst = +prompt("Во сколько обойдется?", ""),
-    itemExpensesSecond = prompt("Введите обязательную статью расходов в этом месяце", ""), 
-    costItemExpensesSecond = +prompt("Во сколько обойдется?", "");
+    time  =  prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
     moneyData: money,
     timeData: time,
-    expenses: {
-        itemExpensesFirst: costItemExpensesFirst,
-        itemExpensesSecond: costItemExpensesSecond
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
 
+// for (let i = 0; i < 2; i++) {
+   
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = +prompt("Во сколько обойдется?", "");
+    
+//     if ( (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50 ) {
+//         appData.expenses[a] = b;
 
-alert("Бюджет на день:" + appData.moneyData / 30);
+//     } else {
+//         alert("Попробуйте еще раз!!!");
+//         i--;
+//     }
+    
+// }
 
-console.log(appData.expenses.itemExpensesFirst);
+let count = 0;
+
+while(count < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = +prompt("Во сколько обойдется?", "");
+
+    if ( (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50 ) {
+        appData.expenses[a] = b;
+        count++;
+
+    } else {
+        alert("Попробуйте еще раз!!!");
+    }
+}
+
+// count = 0;
+
+// do {
+
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = +prompt("Во сколько обойдется?", "");
+
+//     if ( (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50 ) {
+//         appData.expenses[a] = b;
+//         count++;
+
+//     } else {
+//         alert("Попробуйте еще раз!!!");
+//     } 
+
+// } while(count < 2);
+
+appData.moneyPerDay = appData.moneyData / 30;
+
+alert("Ежедненый бюджет: " + appData.moneyPerDay + "Руб.");
+
+if(appData.moneyPerDay < 100){
+    alert("Минимальный уровень достатка");
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    alert("Средний уровень достатка");
+} else if(appData.moneyPerDay > 2000) {
+    alert("Высокий уровень достатка");
+} else{
+    alert("Что то пошло не так!");
+}
+
