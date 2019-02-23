f68baa4a0c07dbc53d0ae1b5cbff01ff1f3610f9
Kolya <nicolay.crasicov@gmail.com>
2019-02-23 23:48:31 +0200

    Приложение. Часть 1 (комит 2)

--- Task 1/script.js
+++ Task 1/script.js
@@ -12,35 +12,35 @@ let appData = {
     savings: false
 };
 
-// for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 2; i++) {
    
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//         b = +prompt("Во сколько обойдется?", "");
-    
-//     if ( (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50 ) {
-//         appData.expenses[a] = b;
-
-//     } else {
-//         alert("Попробуйте еще раз!!!");
-//         i--;
-//     }
-    
-// }
-
-let count = 0;
-
-while(count < 2) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
         b = +prompt("Во сколько обойдется?", "");
-
+    
     if ( (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50 ) {
         appData.expenses[a] = b;
-        count++;
 
     } else {
         alert("Попробуйте еще раз!!!");
+        i--;
     }
+    
 }
+
+// let count = 0;
+
+// while(count < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = +prompt("Во сколько обойдется?", "");
+
+//     if ( (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50 ) {
+//         appData.expenses[a] = b;
+//         count++;
+
+//     } else {
+//         alert("Попробуйте еще раз!!!");
+//     }
+// }
 
 // count = 0;
 
